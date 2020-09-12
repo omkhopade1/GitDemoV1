14cf76441f65a883775e7048a43c825d5b63735b
Pc <Pc@Pc-PC>
2020-09-12 09:57:53 +0530

    This is second commit on the project

--- DemoGitV1/src/DemoClassV2.java
+++ DemoGitV1/src/DemoClassV2.java
@@ -5,6 +5,9 @@ public class DemoClassV2 {
 
 		System.out.println("This is updated class and ready to push to the new branch");
 		System.out.println("Raised the pull Request");
+		
+		
+		System.out.println("This is updated code into the repository");
 	}
 
 }

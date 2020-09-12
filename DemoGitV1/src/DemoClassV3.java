d4efe54c5fb77abdaf7c98b2e87b9ebb84cab35b
Pc <Pc@Pc-PC>
2020-09-12 10:12:10 +0530

    This is the new Branch created for the uploading code

---
+++ DemoGitV1/src/DemoClassV3.java
@@ -0,0 +1,18 @@
+
+public class DemoClassV3 {
+
+	public static void main(String[] args) {
+
+		String hungry= "Yes";
+		
+		if(hungry=="yes")
+		{
+			System.out.println("eat food");
+		}
+		else
+		{
+			System.out.println("Do work coz it is important....");
+		}
+	}
+
+}
